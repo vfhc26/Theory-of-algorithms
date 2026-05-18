a26086db0aa3a19d42985fe965ced18510f15399
arsen <arsenmihajlov983@gmail.com>
2026-05-19 00:19:08 +0300

    примитивно рекурсивная функция на примере функции проверки на четность

---
+++ second/prf.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+// Примитивные рекурсивные функции
+int Z(int x) {return 0;}
+int S(int x) {return x + 1;}
+int Pr(int x) {return x;}
+
+int C1(int x) {return S(Z(x));}
+
+int P(int x) {
+    if (x == 0) return Z(x);
+    return x - 1;
+}
+
+int sub(int x, int y) {
+    if (y == 0) return x;
+    return P(sub(x, y - 1));
+}
+int isEven(int x) {
+    if (x == 0) return 1;
+    return sub(C1(x), isEven(x - 1));
+}
+
+int main(){
+    std::vector<std::pair<int, int>> test_cases = {
+            {0, 1},
+            {11, 0},
+            {2, 1},
+            {39, 0},
+            {400, 1},
+            {67, 0},
+            {100, 1},
+            {11, 0},
+    };
+    for (const auto& test : test_cases) {
+        int x = test.first;
+        int expected = test.second;
+        int result = isEven(x);
+        std::cout << "isEven(" << x << ") = " << result << " (expected: " << expected << ")" << std::endl;
+    }
+}
+
+
